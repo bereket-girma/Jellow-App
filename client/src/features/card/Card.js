24288988febe63a05e7e652105113da1b2502f11
Will Stoddard <wstoddard89@gmail.com>
2020-10-22 14:58:06 -0700

    make folders and files in vscode

---
+++ client/src/features/card/Card.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import styles from './Card.module.css';
+
+
+
+export function Card() {
+
+
+return (
+  <div className="card-container">
+    <div></div>
+  </div>
+
+)
+
+
+
+
+}
