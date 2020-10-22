6b5f9fba4cbcc6eb3b4e015a9dce50836674c64a
Will Stoddard <wstoddard89@gmail.com>
2020-10-22 15:23:25 -0700

    make folders and files in vscode

---
+++ client/src/features/column/Column.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import styles from './Column.module.css';
+
+
+
+export function Column() {
+
+
+
+  return (
+    <div className="column-container">
+
+    </div>
+
+
+  )
+
+
+}
