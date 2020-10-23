ab2df4527563f08f07aed613bd35c58ec1cd8bd2
Will Stoddard <wstoddard89@gmail.com>
2020-10-22 22:26:06 -0700

    make layout for card. style header and start of content

--- client/src/features/card/Card.js
+++ client/src/features/card/Card.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
-import styles from './Card.module.css';
-
-
+import React, { useState } from "react"
+import styles from "./Card.module.css"
 
 export function Card() {
-
-
-return (
-  <div className="card-container">
-    <div></div>
-  </div>
-
-)
-
-
-
-
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.cardDeleteBtn}>x</div>
+      <div className={styles.cardHeader}>
+        <h3 className={styles.cardTitle}>Test Card</h3>
+        <span className={styles.cardIcon}><i class="fas fa-credit-card"></i></span>
+        <div className={styles.cardHeaderInList}>
+          <p>in list
+            <a href="#">To-Do</a>
+          </p>
+        </div>
+      </div>
+      <div className={styles.cardContent}>
+        <div className={styles.cardMembers}>
+          <div>
+            <p>Members</p>
+            <div className={styles.cardAddMembers}><span>+</span></div>
+          </div>
+        </div>
+      <div className={styles.cardSidebar}></div>
+      </div>
+      </div>
+    
+  )
 }
