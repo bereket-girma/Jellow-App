3442b7d69c089a69457d990c6e8a9d0d2acd3dac
dbaluyut <derriquebaluyut@gmail.com>
2020-10-23 13:17:51 -0700

    add auth route

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import { Counter } from "./features/counter/Counter"
+import "./App.css"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom"
+import AuthRoute from "./features/authentication/AuthRoute"
+import { useHistory } from "react-router-dom"
+import { useAuth } from "./features/authentication/auth"
+import api from "./utils/request"
+
+function About() {
+  return <h1>about</h1>
+}
+function Topics() {
+  return <h1>topics</h1>
+}
+function Home() {
+  return <h1>home</h1>
+}
+
+function Login() {
+  const history = useHistory()
+  const { login } = useAuth()
+  function handle(e) {
+    e.preventDefault()
+    login("testuser", "testpassword").then((resp) => {
+      history.push("/about")
+    })
+  }
+  return (
+    <form onSubmit={handle}>
+      <input type="text" />
+      <input type="password" />
+      <button type="submit">submit</button>
+    </form>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <AuthRoute path="/about">
+            <About />
+          </AuthRoute>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
