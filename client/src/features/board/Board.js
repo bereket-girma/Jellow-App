87c9cc60120c20edbc43d2a74c67cc73283fe84d
Will Stoddard <wstoddard89@gmail.com>
2020-10-23 11:53:54 -0700

    make style for board

---
+++ client/src/features/board/Board.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import styles from './Board.module.css';
+
+
+
+export function Board() {
+
+
+  return (
+
+    <div className={styles.boardContainer}>
+      <div className={styles.boardHeader}>Jellow
+        <button className={styles.logOutBtn}>Log Out</button>
+        <div className={styles.avatar}></div>
+      </div>
+      <div className={styles.boardContent}>
+        <p className={styles.boardTitle}>Test Title</p>
+        <div className={styles.boardAddList}>+ Add a List</div>
+      </div>
+    </div>
+
+  )
+}
