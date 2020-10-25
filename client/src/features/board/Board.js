778ac6e82eb7507c97aa95fd38fa38efc1911841
dbaluyut <derriquebaluyut@gmail.com>
2020-10-24 19:32:52 -0700

    test

--- client/src/features/board/Board.js
+++ client/src/features/board/Board.js
@@ -1,23 +1,22 @@
-import React, { useState } from 'react';
-import styles from './Board.module.css';
-
-
+import React, { useState } from "react"
+import styles from "./Board.module.css"
+import { Column } from "../column/Column"
 
 export function Board() {
-
-
   return (
-
     <div className={styles.boardContainer}>
-      <div className={styles.boardHeader}>Jellow
+      <div className={styles.boardHeader}>
+        Jellow
         <button className={styles.logOutBtn}>Log Out</button>
         <div className={styles.avatar}></div>
       </div>
       <div className={styles.boardContent}>
         <p className={styles.boardTitle}>Test Title</p>
-        <div className={styles.boardAddList}><span className={styles.addListSpan}>+</span> Add a List</div>
+        <div className={styles.boardAddList}>
+          <span className={styles.addListSpan}>+</span> Add a List
+        </div>
+        <Column />
       </div>
     </div>
-
   )
 }
