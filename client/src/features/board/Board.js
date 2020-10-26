8a25c1fa50022f234b2a306d2121cc2dbd888531
Bereket Girma <bgirma@Berekets-MacBook-Pro.local>
2020-10-25 20:08:33 -0700

    added some logout fn

--- client/src/features/board/Board.js
+++ client/src/features/board/Board.js
@@ -1,23 +1,42 @@
-import React, { useState } from 'react';
-import styles from './Board.module.css';
-
-
-
+import React, { useState } from "react";
+import styles from "./Board.module.css";
+import { useHistory } from "react-router-dom";
+import { useAuth } from "../authentication/auth";
+import Logout from "../Logout/Logout";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
 export function Board() {
-
+  // function LogoutPage() {
+  const { logout } = useAuth();
+  const { history } = useHistory();
+  function handleClick() {
+    logout().then((resp) => {
+      console.log("some sort of test");
+      history.push("/logout");
+    });
+  }
 
   return (
-
     <div className={styles.boardContainer}>
-      <div className={styles.boardHeader}>Jellow
-        <button className={styles.logOutBtn}>Log Out</button>
+      <div className={styles.boardHeader}>
+        Jellow
+        <button className={styles.logOutBtn} onClick={handleClick}>
+          Log Out
+        </button>
         <div className={styles.avatar}></div>
       </div>
       <div className={styles.boardContent}>
         <p className={styles.boardTitle}>Test Title</p>
-        <div className={styles.boardAddList}><span className={styles.addListSpan}>+</span> Add a List</div>
+        <div className={styles.boardAddList}>
+          <span className={styles.addListSpan}>+</span> Add a List
+        </div>
       </div>
     </div>
-
-  )
+  );
 }
--- server/dbConnect.js
+++ server/dbConnect.js
@@ -3,7 +3,7 @@ const knex = require("knex")({
   version: "7.2",
   connection: {
     host: "127.0.0.1",
-    user: "punchcode",
+    user: "postgres",
     password: "",
     database: "jello_app",
   },
